badc61f075390f6514d753c1d642d014c85582c4
ViktoriaB <v.chamborant@hotmail.com>
2022-01-26 20:31:48 -0600

    Added comments to file

--- main.js
+++ main.js
@@ -8,12 +8,15 @@ function btnClick(){
 
 axios.get('https://swapi.dev/api/planets/2')
 
+//Loop through residents array
   .then(res =>{
      for(let i=0; i < res.data.residents.length; i++){
-        axios.get(res.data.residents[i])
+        axios.get(res.data.residents[i]) //get residents name by index
      .then((res) => {  
     console.log(res.data.name)
        
+    //the way you create a new element and append it to to the document
+    //https://www.w3schools.com/jsref/met_document_createtextnode.asp
     
     let h2 = document.createElement('h2');
     let txt = document.createTextNode(`${res.data.name}`);
